dbad7b499401e8fde2354fa529d54a600e44d2f5
olliekm <revilokm@gmail.com>
2022-06-06 11:25:12 -0400

    fith

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],  theme: {
+      extend: {
+        fontFamily: {
+          'robo': ['Roboto', 'sans-serif'],
+        },
+        colors: {
+          main: "#000411",
+          main2: "#AEB7B3",
+          acc1: "#160C28",
+          acc2: "#EFCB68",
+
+        }
+      },
+  },
+  plugins: [],
+}
